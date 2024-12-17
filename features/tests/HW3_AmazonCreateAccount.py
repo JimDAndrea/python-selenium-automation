fd3454d8331c07212b5a788d4eecb9a891d1032f
Jim D'Andrea <jamesgregorydandrea@hotmail.com>
2024-12-17 09:33:27 -0500

    Update HW3_AmazonCreateAccount.py
    corrections made to the original HW3 submitted code

--- features/tests/HW3_AmazonCreateAccount.py
+++ features/tests/HW3_AmazonCreateAccount.py
@@ -14,25 +14,25 @@ driver = webdriver.Chrome(service=service)
 driver.get('https://www.amazon.com')
 #amazon icon from create account screen
 #HW3-Q1-CSS Selector
-driver.find_element(By.CSS_SELECTOR,"$$('a-icon a-icon-logo')")
+driver.find_element(By.CSS_SELECTOR,"('a-icon a-icon-logo')"
 #HW3-Q2
-driver.find_element(By.CSS_SELECTOR, '$x("//h1[text()='Create account']"')
+driver.find_element(By.CSS_SELECTOR, ("//h1[text()='Create account']")
 #HW3-Q3
-driver.find_element(By.CSS_SELECTOR, '$x("//input[@id='ap_customer_name']")')
+driver.find_element(By.CSS_SELECTOR, ("//input[@id='ap_customer_name']")
 HW3-Q4
-driver.find_element(By.CSS_SELECTOR,'$x("//input[@id='ap_email']")')
+driver.find_element(By.CSS_SELECTOR, ("//input[@id='ap_email']")
 HW3-Q5
-driver.find_element(By.CSS_SELECTOR,'$x("//input[@id='ap_password']")')
+driver.find_element(By.CSS_SELECTOR, ("//input[@id='ap_password']")
 #HW3-Q6
-driver_find_element(By.CSS_SELECTOR, '$x("//input[@placeholder='At least 6 characters']")')
+driver_find_element(By.CSS_SELECTOR, ("//input[@placeholder='At least 6 characters']")
 #HW3-Q7
-driver_find_element(By.CSS_SELECTOR, '$x("//input[@id='ap_password_check']")')
+driver_find_element(By.CSS_SELECTOR, ("//input[@id='ap_password_check']")
 #HW3-Q8
-driver_find_element(By.CSS_SELECTOR,'$x("//input[@id='continue']")')
+driver_find_element(By.CSS_SELECTOR, ("//input[@id='continue']")
 #HW3-Q9
-driver_find_element(By.CSS_SELECTOR,'$x("//a[@href='/gp/help/customer/display.html/ref=ap_register_notification_condition_of_use?ie=UTF8&amp;nodeId=508088']")')
+driver_find_element(By.CSS_SELECTOR, ("//a[@href='/gp/help/customer/display.html/ref=ap_register_notification_condition_of_use?ie=UTF8&amp;nodeId=508088']")
 #HW3-Q10
-driver_find_element(By.CSS_SELECTOR,'$x("//a[@href='/gp/help/customer/display.html/ref=ap_register_notification_privacy_notice?ie=UTF8&amp;nodeId=468496']")')
+driver_find_element(By.CSS_SELECTOR, ("//a[@href='/gp/help/customer/display.html/ref=ap_register_notification_privacy_notice?ie=UTF8&amp;nodeId=468496']")
 #HW3-Q11
-driver_find_element(By.CSS_SELECTOR,'$x("//input[@id='ap_email']")')
+driver_find_element(By.CSS_SELECTOR, ("//input[@id='ap_email']")
 
