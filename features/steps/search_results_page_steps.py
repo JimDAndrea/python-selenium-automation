d3393689ae53ac682db7e382916f2e3412ea778b
JamesDAndrea <jamesgregorydandrea@hotmail.com>
2024-12-27 16:16:56 -0500

    HW5 - Q3

--- features/steps/search_results_page_steps.py
+++ features/steps/search_results_page_steps.py
@@ -1,29 +1,16 @@
 from selenium.webdriver.common.by import By
-from behave import given, when, then
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-#not sure if these next 2 lines are needed
+from behave import given, when, then
 from time import sleep
-from selenium import webdriver
 
 
 ADD_TO_CART_BTN = (By.CSS_SELECTOR, "[id*='addToCartButton']")
-ADD_TO_CART_SIDE_NAV_BTN = (By.XPATH, "//button[@data-test='orderPickupButton']")
-# ADD_TO_CART_SIDE_NAV_BTN = (By.XPATH, "//button[@data-test='shippingButton']")
+ADD_TO_CART_SIDE_NAV_BTN = (By.CSS_SELECTOR, "[data-test='content-wrapper'] [id*='addToCart']")
 PRODUCT_NAME = (By.CSS_SELECTOR, "[data-test='content-wrapper'] h4")
+
 LISTINGS = (By.CSS_SELECTOR, "[data-test*='@web/site-top-of-funnel/ProductCardWrapper']")
-PRODUCT_TITLE = (By.CSS_SELECTOR, "[data-test='productTitle']")
-PRODUCT_IMG = (By.CSS_SELECTOR, "[data-test='productImage']")
-
-
-@given('Open target circle page')
-def open_circle_page(context):
-    context.driver.get('https://www.target.com/circle/')
-    # Wait until the Add to Cart button is clickable
-    wait.until(EC.element_to_be_clickable(ADD_TO_CART_BTN))
-    # click element when available
-    context.driver.find_element(*ADD_TO_CART_BTN).click()
+PRODUCT_TITLE = (By.CSS_SELECTOR, "[data-test='product-title']")
+PRODUCT_IMG = (By.CSS_SELECTOR, 'img')
 
 
 @then('Verify search results shown for {product}')
@@ -32,45 +19,38 @@ def verify_search_results(context, product):
     assert product in actual_result, f'Expected text {product} not in actual {actual_result}'
 
 
-@then('verify number of cells')
-def verify_number_of_cells(context):
-    assert len(context.benefit_cells) == 14, f"Expected 14 benefit cells, but found {len(context.benefit_cells)}."
+@when('Click on Add to Cart button')
+def click_add_to_cart(context):
+    context.driver.find_element(*ADD_TO_CART_BTN).click()  # always clicks on 1st Add to cart btn
+    # context.driver.find_elements(By.CSS_SELECTOR, "[id*='addToCartButton']")[0].click()
+    # context.driver.wait.until(EC.visibility_of_element_located(ADD_TO_CART_SIDE_NAV_BTN))
 
 
-@when('benefit cells are located')
-def benefit_cells_located(context):
-    context.benefit_cells = context.driver.find_elements(By.CSS_SELECTOR,'.cell-item-content')
+@when('Store product name')
+def store_product_name(context):
+    context.product_name = context.driver.find_element(*PRODUCT_NAME).text
+    print(f'Product stored: {context.product_name}')
 
-
-@when('Verify item is in cart')
-def verify_item_in_cart(context):
-    actual_result = context.driver.find_element(By.CSS_SELECTOR, 'span.h-text-lg').text
-    assert product in actual_result, f'Expected text "Added to cart" not in actual {actual_result}'
 
 @when('Confirm Add to Cart button from side navigation')
 def side_nav_click_add_to_cart(context):
-    #properly initializing the wait object for the driver
-    # wait = WebDriverWait(context.driver, 10)
-    context.driver.wait.until(EC.element_to_be_clickable(ADD_TO_CART_SIDE_NAV_BTN))
-    context.driver.find_element(*ADD_TO_CART_SIDE_NAV_BTN).click()
-
-@when("Click on Add to Cart button")
-def click_add_to_cart(context):
-    context.driver.find_element(*ADD_TO_CART_BTN).click()
-
-
-@then ("Verify that every product has a name and an image")
-def verify_products_name_img(context):
-    context.driver.execute_script("window.scrollBy(0,2000)","")
+    context.driver.wait.until(EC.element_to_be_clickable(ADD_TO_CART_SIDE_NAV_BTN)).click()
+    # context.driver.find_element(*ADD_TO_CART_SIDE_NAV_BTN)
     sleep(4)
-    context.driver.execute_script("window.scrollBy(0,2000)","")
-    #Find all products:
-    all_products = context.driver.find_elements(*LISTINGS)
-    # print(all_products)
-    assert len(all_products) > 0, 'No products found!'
 
-    for product in all_products:
-        title = product.find_element(*PRODUCT_TITLE).text
-        assert title != '', 'Product title not shown'
-        print(title)
-        product.find_element(*PRODUCT_IMG)
+@then('Verify that every product has a name and an image')
+def verify_product_name_and_image(context):
+    context.driver.execute_script("window.scrollBy(0, 2000)","")
+    sleep(4)
+    context.driver.execute_script("window.scrollBy(0,2000)", "")
+    # Find all products - question 3 get all products
+    all_products = context.driver.find_elements(*LISTINGS)
+    print(all_products)
+    assert len(all_products) > 0, 'No products found'
+
+    for products in all_products:
+      title = products.find_element(*PRODUCT_TITLE).text
+      # checks that its not an empty string != ''
+      assert title, 'Product title not shown'
+      print (title)
+      products.find_element(*PRODUCT_IMG)
