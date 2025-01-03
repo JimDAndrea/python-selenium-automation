c8c2a8ecd13b0b33d9516d49a3c08e1b2f67d1c7
JamesDAndrea <jamesgregorydandrea@hotmail.com>
2025-01-02 20:08:04 -0500

    Converted to page objects

--- pages/main_page.py
+++ pages/main_page.py
@@ -3,5 +3,5 @@ from pages.base_page import BasePage
 
 class MainPage(BasePage):
 
-    def open_main_page(self):
-        self.open_url('https://www.target.com/')
+    def open_main(self):
+        self.open_url('https://target.com')
