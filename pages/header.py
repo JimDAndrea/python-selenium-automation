ef443477b781b6330e7b35daa32472450c2d4d58
JamesDAndrea <jamesgregorydandrea@hotmail.com>
2025-01-07 13:45:19 -0500

    HW7 Q2

--- pages/header.py
+++ pages/header.py
@@ -8,6 +8,9 @@ class Header(BasePage):
     SEARCH_FIELD = (By.ID, 'search')
     SEARCH_BTN = (By.XPATH, "//button[@data-test='@web/Search/SearchButton']")
     CART_ICON = (By.CSS_SELECTOR, "[data-test='@web/CartLink']")
+    MAIN_SIGN_IN_BTN = (By.CSS_SELECTOR, "[id='account-sign-in']")
+    RT_SIDE_NAV_SIGNIN = (By.CSS_SELECTOR, "[data-test='accountNav-signIn']")
+    SIGN_IN_PAGE_SIGNIN_BTN = (By.CSS_SELECTOR, "[@id='login']")
 
     def search_product(self, product):
         self.input_text(product, *self.SEARCH_FIELD)
@@ -17,3 +20,14 @@ class Header(BasePage):
     def ClickCart(self):
         # self.click(*self.CART_ICON)
         self.wait_and_click(*self.CART_ICON)
+
+    def ClickSignIn(self):
+        self.click(*self.MAIN_SIGN_IN_BTN)
+
+    def ClickSignInFromNav(self):
+        self.click(*self.RT_SIDE_NAV_SIGNIN)
+
+    def VerifySignInForm(self):
+        self.verify_text('Sign in', *self.SIGN_IN_PAGE_SIGNIN_BTN)
+
+
