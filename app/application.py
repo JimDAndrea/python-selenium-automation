eba9400053c934ba1fc250aa06cf519bd1de5531
JamesDAndrea <jamesgregorydandrea@hotmail.com>
2025-01-22 12:01:51 -0500

    HW8 Browserstack test environment

--- app/application.py
+++ app/application.py
@@ -1,6 +1,7 @@
 from pages.base_page import BasePage
 from pages.cart_page import CartPage
 from pages.header import Header
+from pages.login_page import SignInPage
 from pages.main_page import MainPage
 from pages.search_results_page import SearchResultsPage
 
@@ -15,6 +16,7 @@ class Application:
         self.main_page = MainPage(driver)
         self.search_results_page = SearchResultsPage(driver)
         self.cart_page = CartPage(driver)
+        self.login_page = SignInPage(driver)
 
 
 
--- features/tests/main_page_ui.feature
+++ features/tests/main_page_ui.feature
@@ -4,7 +4,7 @@ Feature: Tests for Main page UI
   Scenario: User can see header links
     Given Open target main page
     Then Verify at least 1 header link is shown
-    Then Click Sign In
+    And Click Sign In
     Then From right side Navigation menu Click Sign In
     Then Verify Sign In form opened
 
